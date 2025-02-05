9daa28b27debffffc8e4e24560db348ac6b5cb97
Dave Nesbitt <git@github.com>
2025-02-05 09:23:22 +0100

    Remove some CI steps for now

--- setup.py
+++ setup.py
@@ -2,12 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='autiobooks',
-    version='0.1.0',
+    version='1.0.0',
     packages=find_packages(),
     install_requires=[
     ],
     author='David Nesbitt',
-    author_email='your.email@example.com',
     description='Automatically convert epubs to audiobooks',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
